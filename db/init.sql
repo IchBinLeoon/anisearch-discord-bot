1aa72798ca85a643e58ded773cf2d2132a8b7d53
IchBinLeoon <50367411+IchBinLeoon@users.noreply.github.com>
2021-03-25 12:45:19 +0100

    Add postgres db to docker-compose

---
+++ db/init.sql
@@ -0,0 +1,2 @@
+CREATE TABLE IF NOT EXISTS guilds (id bigint, prefix VARCHAR (5));
+CREATE TABLE IF NOT EXISTS users (id bigint, anilist VARCHAR (255), myanimelist VARCHAR (255), kitsu VARCHAR (255));
