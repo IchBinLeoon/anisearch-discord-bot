5a1b595331e75fea3d78921ac9ef1d435f08ba63
IchBinLeoon <50367411+IchBinLeoon@users.noreply.github.com>
2021-09-08 17:50:10 +0200

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,3 +1,25 @@
-CREATE TABLE IF NOT EXISTS guilds (id bigint, prefix VARCHAR (5), channel bigint, role bigint, watchlist integer[] DEFAULT '{}');
-CREATE TABLE IF NOT EXISTS users (id bigint, anilist VARCHAR (255), myanimelist VARCHAR (255), kitsu VARCHAR (255));
-CREATE TABLE IF NOT EXISTS schedule (id bigint, time bigint, episode int, romaji VARCHAR (255), english VARCHAR (255), image VARCHAR (255), url VARCHAR (255), nsfw boolean);
+CREATE TABLE IF NOT EXISTS guilds (
+    id bigint NOT NULL,
+    prefix varchar(5) NOT NULL,
+    channel bigint,
+    role bigint,
+    watchlist integer[] NOT NULL DEFAULT '{}'
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id bigint NOT NULL,
+    anilist text,
+    myanimelist text,
+    kitsu text
+);
+
+CREATE TABLE IF NOT EXISTS schedule (
+    id bigint,
+    time bigint,
+    episode int,
+    romaji text,
+    english text,
+    image text,
+    url text,
+    nsfw boolean
+);
